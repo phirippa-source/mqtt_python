50254666b869f476fc9e99e6965d531a424fb729
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2024-12-01 08:52:09 +0900

    Create subBasic.py

---
+++ subBasic.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(clinet, userdata, flag, rc):
+    print('Connect with result code : ' + str(rc))
+    clinet.subscribe('temp')
+
+def on_message(client, userdata, msg):
+    print(msg.topic + ':' + str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect('localhost', 1883, 60)
+client.loop_forever()
