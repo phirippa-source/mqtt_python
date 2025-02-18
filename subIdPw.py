edd637c443bb40bdcbfe8a687bdb2888772b353a
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-02-18 10:34:11 +0900

    Id와 패스워드를 가지고 구독 신청

--- subIdPw.py
+++ subIdPw.py
@@ -1,15 +1,14 @@
-
 import paho.mqtt.client as mqtt
 
 def on_connect(clinet, userdata, flag, rc):
     print('Connect with result code : ' + str(rc))
-    clinet.subscribe('MyOffice/#')
+    clinet.subscribe('temp')
 
 def on_message(client, userdata, msg):
     print(msg.topic + ':' + str(msg.payload))
 
 client = mqtt.Client()
-client.username_pw_set(username='mqtt_ship', password='1234')
+client.username_pw_set(username='ship', password='1234')
 
 client.on_connect = on_connect
 client.on_message = on_message
