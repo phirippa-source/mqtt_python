871d77f285dd2705ea837ccfe0ebe5703bde7a7f
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-02-18 21:41:40 +0900

    Update select_datapoint_raw.py

--- select_datapoint_raw.py
+++ select_datapoint_raw.py
@@ -1,5 +1,9 @@
 from influxdb import InfluxDBClient
-client = InfluxDBClient( host='localhost', port='8086', username='ship', password='1234', database='dbData')
+client = InfluxDBClient(  host='localhost',
+                          port='8086',
+                          username='ship',
+                          password='1234',
+                          database='dbData')
 
 result = client.query('select * from sensors')
 print( result.raw )
