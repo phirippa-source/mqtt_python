266a99b92c2556928790edc6f167ab7af7f507d5
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-02-22 15:31:37 +0900

    Update pubArduino.py

--- pubArduino.py
+++ pubArduino.py
@@ -4,19 +4,20 @@ import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
 client.username_pw_set(username='ship', password='1234')
-
-# 포트 이름과 baud rate을 확인하세요
-ser = serial.Serial(port='PORT', baudrate=9600)
-ser.readline()
 client.connect('localhost', 1883, 60)
 
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
+
 while True:
-  line = ser.readline().decode('utf-8')
-  try:
-    dic = json.loads(line[:-1])
-  except:
-    print('Error!')
-  else:
-    print(dic)
-    for key, value in dic.items():
-      client.publish(key, value)
+  if ser.readable():
+    line = ser.readline().decode('utf-8')[:-1]
+    try:
+      message = json.loads(line)
+    except:
+      pass
+    else:
+      # print("message:", message)
+      for topic, payload in message.items():
+        client.publish(topic, payload)
+        print(f'publishing - topic:{topic} \tpayload:{payload}')
+      print()
