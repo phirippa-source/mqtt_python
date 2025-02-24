c5ada40ca49b6adcfa66b0783c32ae3652f989d2
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-02-24 22:04:55 +0900

    Update getDataFromArduino.py

--- getDataFromArduino.py
+++ getDataFromArduino.py
@@ -6,6 +6,6 @@ ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
 while True:
   line = ser.readline().decode('utf-8')[:-1]
   try:
-    print("payload:", json.loads(line)
+    print("payload:", json.loads(line))
   except:
     pass
