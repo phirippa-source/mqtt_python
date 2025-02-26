48d1fc5f002cffa820b15fa43e1264270dd9ab56
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-02-26 19:44:41 +0900

    Update pubArduinoUpGrade.py

--- pubArduinoUpGrade.py
+++ pubArduinoUpGrade.py
@@ -5,6 +5,7 @@ import paho.mqtt.client as mqtt
 client = mqtt.Client()
 client.username_pw_set(username='ship', password='1234')
 ser = serial.Serial(port='PORT', baudrate=9600)
+ser.readline()
 topic = 'MyOffice/Indoor/Values'
 client.connect('localhost', 1883, 60)
 
