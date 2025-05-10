ecb430dd2435aa21d2e20905d588e03801f3c87e
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-05-10 14:08:53 +0900

    Update pubTopic.py

--- pubTopic.py
+++ pubTopic.py
@@ -6,11 +6,13 @@ client.connect('localhost', 1883, 60)
 
 for i in range(3):
     client.publish('MyOffice/Indoor/Temp', 25.1)
+    print('MyOffice/Indoor/Temp', 25.1)
     client.publish('MyOffice/Indoor/Humi', 33)
-    client.publish('MyOffice/Indoor/Lux', 49)
-
+    print('MyOffice/Indoor/Humi', 33)
+        
     client.publish('MyOffice/Outdoor/Temp', 33.9)
+    print('MyOffice/Outdoor/Temp', 33.9)    
     client.publish('MyOffice/Outdoor/Humi', 23)
-    client.publish('MyOffice/Outdoor/Lux', 30)
-
+    print('MyOffice/Outdoor/Humi', 23)
     time.sleep(2)
+
